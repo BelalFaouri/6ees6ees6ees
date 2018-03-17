eb5b229bd843809eac2db4da331c77d4809ef596
yussur90 <yuyualani@yahoo.com>
2018-03-17 19:28:58 +0200

    final6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+constructor(age,color,treasureChest){
+super(age,color,treasureChest)
+this.age=40
+this.job='gamble';
+this.canFly=false;
+this.color='grey'
+}
+forage(){
+return 'I am too old, let me play cards instead'
+}
+gamble(value){
+	this.treasureChest.push(value)
+}
 };
